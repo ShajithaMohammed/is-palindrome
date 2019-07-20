6e07ceb9609a212b97411520211fa947dbd3f96e
ShajithaMohammed <shajithaparveen28@gmail.com>
2019-07-20 12:46:20 +0530

    Using built-in function.
    Rename

--- src/main/java/Palindrome.java
+++ src/main/java/Palindrome.java
@@ -1,14 +1,6 @@
 public class Palindrome {
     public boolean check(String sentence) {
-        if(sentence==null)
-            return false;
-
-        String fmtSnt = sentence.replaceAll("\\W", "").toLowerCase();
-
-        for (int i = 0; i < fmtSnt.length() / 2; i++) {
-            if (fmtSnt.charAt(i) != fmtSnt.charAt(fmtSnt.length() - i - 1))
-                return false;
-        }
-        return true;
+        String fmtSnt = sentence.replaceAll("\\W", "");
+        return fmtSnt.equalsIgnoreCase(new StringBuilder(fmtSnt).reverse().toString());
     }
 }
